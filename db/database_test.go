aecd54f923c31795ed898b7bca62d1fdf2b8df99
zhangjun <zhangjun@juzix.net>
2021-06-02 11:53:23 +0800

    newTestLDB for database_test.go

--- db/database_test.go
+++ db/database_test.go
@@ -1,2 +1,22 @@
 package db_test
 
+import (
+	"github.com/RosettaFlow/Carrier-Go/db"
+	"io/ioutil"
+	"os"
+)
+
+func newTestLDB() (*db.LDBDatabase, func()) {
+	dirname, err := ioutil.TempDir(os.TempDir(), "db_test_")
+	if err != nil {
+		panic("failed to create test file: " + err.Error())
+	}
+	db, err := db.NewLDBDatabase(dirname, 0, 0)
+	if err != nil {
+		panic("failed to create test database: " + err.Error())
+	}
+	return db, func() {
+		db.Close()
+		os.RemoveAll(dirname)
+	}
+}
