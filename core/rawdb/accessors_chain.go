810b3b1fcc7005397d9af03474be52ec0cdc3d51
zhangjun <zhangjun@juzix.net>
2021-06-04 15:33:10 +0800

    more operation to write/read/delete database

--- core/rawdb/accessors_chain.go
+++ core/rawdb/accessors_chain.go
@@ -3,6 +3,7 @@ package rawdb
 import (
 	"encoding/binary"
 	"github.com/RosettaFlow/Carrier-Go/common"
+	"github.com/RosettaFlow/Carrier-Go/lib/types"
 )
 
 // ReadDataHash retrieves the hash assigned to a canonical block number
@@ -68,4 +69,144 @@ func WriteHeadBlockHash(db DatabaseWriter, hash common.Hash) {
 	if err := db.Put(headBlockKey, hash.Bytes()); err != nil {
 		log.WithError(err).Error("Failed to store last block's hash")
 	}
+}
+
+// ReadHeaderPB retrieves a block header in its raw RLP database encoding.
+func ReadHeaderPB(db DatabaseReader, hash common.Hash, number uint64) []byte {
+	data, _ := db.Get(headerKey(number, hash))
+	return data
+}
+
+// HasHeader verifies the existence of a block header corresponding to the hash.
+func HasHeader(db DatabaseReader, hash common.Hash, number uint64) bool {
+	if has, err := db.Has(headerKey(number, hash)); !has || err != nil {
+		return false
+	}
+	return true
+}
+
+// ReadHeader retrieves the block header corresponding to the hash.
+func ReadHeader(db DatabaseReader, hash common.Hash, number uint64) *types.Header {
+	// todo: protobuf or rlp, consider.............
+	data := ReadHeaderPB(db, hash, number)
+	if len(data) == 0 {
+		return nil
+	}
+	header := new(types.Header)
+	if err := header.Unmarshal(data); err != nil {
+		log.WithField("hash", hash).WithError(err).Error("Invalid block header ProtoBuf")
+		return nil
+	}
+	return header
+}
+
+// WriteHeader stores a block header into the database and also stores the hash-to-number mapping.
+func WriteHeader(db DatabaseWriter, header *types.Header) {
+	var (
+		hash 	= common.HexToHash(header.Hash)
+		number 	= header.Version
+		encoded = encodeNumber(number)
+	)
+	key := headerNumberKey(hash)
+	if err := db.Put(key, encoded); err != nil {
+		log.WithError(err).Error("Failed to store hash to number mapping")
+	}
+	data, err := header.Marshal()
+	if err != nil {
+		log.WithError(err).Error("Failed to PB encode header")
+	}
+	key  = headerKey(number, hash)
+	if err := db.Put(key, data); err != nil {
+		log.WithError(err).Error("Failed to store header")
+	}
+}
+
+// DeleteHeader remove all block header data associated with a hash.
+func DeleteHeader(db DatabaseDeleter, hash common.Hash, number uint64) {
+	if err := db.Delete(headerKey(number, hash)); err != nil {
+		log.WithError(err).Error("Failed to delete header")
+	}
+	if err := db.Delete(headerNumberKey(hash)); err != nil {
+		log.WithError(err).Error("Failed to delete hash to number mapping")
+	}
+}
+
+// ReadBodyPB retrieves the block body (metadata/resource/identity/task) in Protobuf encoding.
+func ReadBodyPB(db DatabaseReader, hash common.Hash, number uint64) []byte {
+	data, _ := db.Get(blockBodyKey(number, hash))
+	return data
+}
+
+// WriteBodyPB stores an Protobuf encoded block body into the database.
+func WriteBodyPB(db DatabaseWriter, hash common.Hash, number uint64, pb []byte) {
+	if err := db.Put(blockBodyKey(number, hash), pb); err != nil {
+		log.WithError(err).Error("Failed to store block body")
+	}
+}
+
+// HasBody verifies the existence of a block body corresponding to the hash.
+func HasBody(db DatabaseReader, hash common.Hash, number uint64) bool {
+	if has, err := db.Has(blockBodyKey(number, hash)); !has || err != nil {
+		return false
+	}
+	return true
+}
+
+// ReadBody retrieves the block body corresponding to the hash.
+func ReadBody(db DatabaseReader, hash common.Hash, number uint64) *types.BodyData {
+	data := ReadBodyPB(db, hash, number)
+	if len(data) == 0 {
+		return nil
+	}
+	body := new(types.BodyData)
+	if err := body.Unmarshal(data); err != nil {
+		log.WithField("hash", hash).WithError(err).Error("Invalid block body Protobuf")
+		return nil
+	}
+	return body
+}
+
+// WriteBOdy store a block body into the database.
+func WriteBody(db DatabaseWriter, hash common.Hash, number uint64, body *types.BodyData) {
+	data, err := body.Marshal()
+	if err != nil {
+		log.WithError(err).Error("Failed to ProtoBuf encode body")
+	}
+	WriteBodyPB(db, hash, number, data)
+	// todo: Data analysis is required when storing the body.
+	// metadata/resource/identity/...
+}
+
+// DeleteBody removes all block body data associated with a hash.
+func DeleteBody(db DatabaseDeleter, hash common.Hash, number uint64) {
+	if err := db.Delete(blockBodyKey(number, hash)); err != nil {
+		log.WithError(err).Error("Failed to delete block body")
+	}
+	// todo: remove metadata/resource/identity/task at the same time.
+}
+
+func ReadBlock(db DatabaseReader, hash common.Hash, number uint64) *types.BlockData {
+	header := ReadHeader(db, hash , number)
+	if header == nil {
+		return nil
+	}
+	body := ReadBody(db, hash, number)
+	if body == nil {
+		return nil
+	}
+	// todo: generated block by header and body.
+	// return types.NewBlockWithHeader(header).WithBody(body.Transactions, body.ExtraData)
+	return nil
+}
+
+// WriteBlock serializes a block into the database, header and body separately.
+func WriteBlock(db DatabaseWriter, block *types.BlockData) {
+	//WriteBody(db, block.Hash(), block.NumberU64(), block.Body())
+	//WriteHeader(db, &block.Header)
+}
+
+// DeleteBlock removes all block data associated with a hash.
+func DeleteBlock(db DatabaseDeleter, hash common.Hash, number uint64) {
+	DeleteHeader(db, hash, number)
+	DeleteBody(db, hash, number)
 }
