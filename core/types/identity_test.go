44b91dc8a7e28c88981b235605a20d1f6ce1edf3
zhangjun <zhangjun@juzix.net>
2021-06-07 15:22:16 +0800

    test for identity

---
+++ core/types/identity_test.go
@@ -0,0 +1,41 @@
+package types
+
+import (
+	"bytes"
+	"github.com/RosettaFlow/Carrier-Go/common"
+	"github.com/RosettaFlow/Carrier-Go/lib/types"
+	"testing"
+)
+
+var identities = NewIdentity(&types.IdentityData{
+	Identity:             "",
+	NodeId:               "",
+	NodeName:             "",
+	DataId:               "",
+	DataStatus:           "",
+	Status:               "",
+	Credential:           "",
+	XXX_NoUnkeyedLiteral: struct{}{},
+	XXX_unrecognized:     nil,
+	XXX_sizecache:        0,
+})
+
+func TestIdentitiesEncode(t *testing.T) {
+	buffer := new(bytes.Buffer)
+	err := identities.EncodePb(buffer)
+	if err != nil {
+		t.Fatal("identity encode protobuf failed, err: ", err)
+	}
+
+	didentities := new(Identity)
+	err = didentities.DecodePb(buffer.Bytes())
+	if err != nil {
+		t.Fatal("identity decode protobuf failed, err: ", err)
+	}
+	dBuffer := new(bytes.Buffer)
+	didentities.EncodePb(dBuffer)
+
+	if !bytes.Equal(buffer.Bytes(), dBuffer.Bytes()) {
+		t.Fatalf("identity encode protobuf mismatch, got %x, want %x", common.Bytes2Hex(dBuffer.Bytes()), common.Bytes2Hex(buffer.Bytes()))
+	}
+}
