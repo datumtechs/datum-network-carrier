862e1fb4c3cda4bc76db0bb5ae298d0e463f9724
Gavin <meiyan532484710@163.com>
2021-06-09 14:49:12 +0800

    添加了点主干代码 Signed-off-by: Gavin <meiyan532484710@163.com>

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -1,31 +1,57 @@
 package rpc
 
 import (
-	rpcapi "github.com/RosettaFlow/Carrier-Go/lib/api"
+	"github.com/RosettaFlow/Carrier-Go/common"
+	pb "github.com/RosettaFlow/Carrier-Go/lib/api"
 	"google.golang.org/grpc"
 	"log"
 	"net"
 )
 
-type RpcServer struct {
-
+type RpcApiServer struct {
+	Cfg 				*common.RpcConfig
+	GrpcSvr 			*grpc.Server
+	b             		Backend
 }
 
-func (svr *RpcServer) Start() {
 
-	lis, err := net.Listen("tcp", "port")
+func New(cfg *common.RpcConfig, b Backend) *RpcApiServer {
+	return &RpcApiServer{Cfg: cfg, b: b}
+}
+func (svr *RpcApiServer) Start() {
+
+	lis, err := net.Listen(svr.Cfg.Protocol, svr.Cfg.Ip+":"+svr.Cfg.Port)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("Failed to start rpc server, listen failed: %v", err)
 	}
 
 	var opts []grpc.ServerOption
 
 	s := grpc.NewServer(opts...)
 
+	pb.RegisterYarnServiceServer(s, &yarnServiceServer{b: svr.b})
+	pb.RegisterMetaDataServiceServer(s, &metaDataServiceServer{b: svr.b})
+	pb.RegisterPowerServiceServer(s, &powerServiceServer{b: svr.b})
+	pb.RegisterAuthServiceServer(s, &authServiceServer{b: svr.b})
+	pb.RegisterTaskServiceServer(s, &taskServiceServer{b: svr.b})
 
-	rpcapi.RegisterYarnServiceServer(s, &rpcapi.YarnServiceServer{})
-
-	fmt.Printf("Start Server :%s ... \n", port)
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		log.Fatalf("Failed to start rpc server, serve failed: %v", err)
+	}
+	svr.GrpcSvr = s
 }
+
+func (svr *RpcApiServer) Stop () {
+	svr.GrpcSvr.Stop()
+	log.Println("Stop grpc server ...")
+}
+
+func (svr *RpcApiServer) GracefulStop () {
+	svr.GrpcSvr.GracefulStop()
+	log.Println("Stop grpc server graceful ...")
+}
+
+func (svr *RpcApiServer) GetServiceInfo() map[string]grpc.ServiceInfo {
+	return svr.GrpcSvr.GetServiceInfo()
+}
+
