28b3e4e98fe3eb0b36e8c9244dece74ebab4a433
zhangjun <zhangjun@juzix.net>
2021-06-09 12:01:44 +0800

    datacenter and config

---
+++ core/datacenter.go
@@ -0,0 +1,72 @@
+package core
+
+import (
+	"fmt"
+	"github.com/RosettaFlow/Carrier-Go/core/types"
+	"github.com/RosettaFlow/Carrier-Go/grpclient"
+	"github.com/RosettaFlow/Carrier-Go/params"
+	lru "github.com/hashicorp/golang-lru"
+	"sync"
+	"sync/atomic"
+)
+
+// DataCenter is mainly responsible for communicating with the data center service
+type DataCenter struct {
+	config 			*params.DataCenterConfig
+	client 			*grpclient.GrpcClient
+	mu     			sync.RWMutex 	// global mutex for locking data center operations.
+	procmu 			sync.RWMutex 	// data processor lock
+
+	blockCache  	*lru.Cache
+	bodyCache   	*lru.Cache
+	bodyPbCache 	*lru.Cache
+
+	processor 		Processor // block processor interface
+
+	quit        	chan struct{} 	// datachain quit channel
+	running     	int32         	// running must be called atomically
+	procInterrupt 	int32          	// interrupt signaler for block processing
+	wg            	sync.WaitGroup 	// chain processing wait group for shutting down
+}
+
+// NewDataCenter returns a fully initialised data center using information available in the database.
+func NewDataCenter(config *params.DataCenterConfig) (*DataCenter, error) {
+	blockCache, _ := lru.New(blockCacheLimit)
+	bodyCache, _ := lru.New(blockCacheLimit)
+	bodyPbCache, _ := lru.New(blockCacheLimit)
+	// todo: When to call Close??
+	client, err := grpclient.Dial(fmt.Sprintf("%v:%v", config.GrpcUrl, config.Port))
+	if err != nil {
+		log.WithError(err).Error("dial grpc server failed")
+		return nil, err
+	}
+	dc := &DataCenter{
+		config: 		config,
+		client: 		client,
+		quit:          	make(chan struct{}),
+		blockCache:    	blockCache,
+		bodyCache:     	bodyCache,
+		bodyPbCache:   	bodyPbCache,
+	}
+	return dc, nil
+}
+
+func (dc *DataCenter) getProcInterrupt() bool {
+	return atomic.LoadInt32(&dc.procInterrupt) == 1
+}
+
+func (dc *DataCenter) SetProcessor(processor Processor) {
+	dc.procmu.Lock()
+	defer dc.procmu.Unlock()
+	// do setting...
+	dc.processor = processor
+}
+
+
+
+func (dc *DataCenter) Insert(block *types.Block) {
+
+}
+
+
+
