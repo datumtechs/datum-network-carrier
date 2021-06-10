20c5067ef20241f3bae91d886fc9db01ccc2181f
zhangjun <zhangjun@juzix.net>
2021-06-10 11:59:48 +0800

    app entrance

---
+++ cmd/carrier/flags/flags.go
@@ -0,0 +1,60 @@
+package flags
+
+import (
+	"github.com/RosettaFlow/Carrier-Go/common/fileutil"
+	"github.com/urfave/cli/v2"
+	"path/filepath"
+	"runtime"
+)
+
+var (
+	// RPCHost defines the host on which the RPC server should listen.
+	RPCHost = &cli.StringFlag{
+		Name:  "rpc-host",
+		Usage: "Host on which the RPC server should listen",
+		Value: "127.0.0.1",
+	}
+	// RPCPort defines a beacon node RPC port to open.
+	RPCPort = &cli.IntFlag{
+		Name:  "rpc-port",
+		Usage: "RPC port exposed by a beacon node",
+		Value: 4000,
+	}
+	// GRPCGatewayHost specifies a gRPC gateway host for Carrier.
+	GRPCGatewayHost = &cli.StringFlag{
+		Name:  "grpc-gateway-host",
+		Usage: "The host on which the gateway server runs on",
+		Value: "127.0.0.1",
+	}
+	// GRPCGatewayPort enables a gRPC gateway to be exposed for Carrier.
+	GRPCGatewayPort = &cli.IntFlag{
+		Name:  "grpc-gateway-port",
+		Usage: "Enable gRPC gateway for JSON requests",
+		Value: 3500,
+	}
+
+	// DataDirFlag defines a path on disk.
+	DataDirFlag = &cli.StringFlag{
+		Name:  "datadir",
+		Usage: "Data directory for the databases and keystore",
+		Value: DefaultDataDir(),
+	}
+)
+
+// DefaultDataDir is the default data directory to use for the databases and other
+// persistence requirements.
+func DefaultDataDir() string {
+	// Try to place the data folder in the user's home dir
+	home := fileutil.HomeDir()
+	if home != "" {
+		if runtime.GOOS == "darwin" {
+			return filepath.Join(home, "Library", "Eth2")
+		} else if runtime.GOOS == "windows" {
+			return filepath.Join(home, "AppData", "Local", "Eth2")
+		} else {
+			return filepath.Join(home, ".eth2")
+		}
+	}
+	// As we cannot guess a stable location, return empty and handle later
+	return ""
+}
