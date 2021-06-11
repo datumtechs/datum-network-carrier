97592ed28bc7b5e04152cbff36e14251c75ad7ed
zjsunzone <zjsunzone@gmail.com>
2021-06-11 15:33:01 +0800

    service define

--- proto/generate.go
+++ proto/generate.go
@@ -21,7 +21,7 @@ import (
 
 // Inception, go generate calls the script itself that then deals with generation.
 // This is only done because go:generate does not support wildcards in paths.
-//go:generate go run generate.go lib/db lib/types lib/api
+//go:generate go run generate.go lib/db lib/types lib/api lib/center/api
 
 func main() {
 	for _, path := range os.Args[1:] {
