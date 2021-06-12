4e8d8455f1f8943d907df1526428e5c5608a73a2
Gavin <meiyan532484710@163.com>
2021-06-12 16:36:07 +0800

    Signed-off-by: Gavin <meiyan532484710@163.com>

--- event/events.go
+++ event/events.go
@@ -2,6 +2,14 @@ package event
 
 import "github.com/RosettaFlow/Carrier-Go/types"
 
-type MetaDataMsgEvent struct {Msgs types.MetaDataMsgs}
-type PowerMsgEvent struct {Msgs types.PowerMsgs}
-type TaskMsgEvent struct {Msgs types.TaskMsgs}
+type MetaDataMsgEvent struct{ Msgs types.MetaDataMsgs }
+type PowerMsgEvent struct{ Msgs types.PowerMsgs }
+type TaskMsgEvent struct{ Msgs types.TaskMsgs }
+
+type TaskEvent struct {
+	Type       string `json:"type"`
+	Identity   string `json:"identity"`
+	TaskId     string `json:"taskId"`
+	Content    string `json:"content"`
+	CreateTime string `json:"createTime"`
+}
