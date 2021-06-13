dcdc24d6b34f6d3155e7a285d683ef357bf91bf6
zhangjun <zhangjun@juzix.net>
2021-06-13 10:16:22 +0800

    update api_backend.go

--- carrier/api_backend.go
+++ carrier/api_backend.go
@@ -15,35 +15,35 @@ func (s *CarrierAPIBackend) SendMsg(msg types.Msg) error {
 }
 
 func (s *CarrierAPIBackend) SetSeedNode(seed *types.SeedNodeInfo) (types.NodeConnStatus, error) {
-	return types.NONCONNECTED, nil
+	return s.carrier.datachain.SetSeedNode(seed)
 }
 
 func (s *CarrierAPIBackend) DeleteSeedNode(id string) error {
-	return nil
+	return s.carrier.datachain.DeleteSeedNode(id)
 }
 
 func (s *CarrierAPIBackend) GetSeedNode(id string) (*types.SeedNodeInfo, error) {
-	return nil, nil
+	return s.carrier.datachain.GetSeedNode(id)
 }
 
 func (s *CarrierAPIBackend) GetSeedNodeList() ([]*types.SeedNodeInfo, error) {
-	return nil, nil
+	return s.carrier.datachain.GetSeedNodeList()
 }
 
 func (s *CarrierAPIBackend) SetRegisterNode(typ types.RegisteredNodeType, node *types.RegisteredNodeInfo) (types.NodeConnStatus, error) {
-	return types.NONCONNECTED, nil
+	return s.carrier.datachain.SetRegisterNode(typ, node)
 }
 
 func (s *CarrierAPIBackend) DeleteRegisterNode(typ types.RegisteredNodeType, id string) error {
-	return nil
+	return s.carrier.datachain.DeleteRegisterNode(typ, id)
 }
 
 func (s *CarrierAPIBackend) GetRegisterNode(typ types.RegisteredNodeType, id string) (*types.RegisteredNodeInfo, error) {
-	return nil, nil
+	return s.carrier.datachain.GetRegisterNode(typ, id)
 }
 
 func (s *CarrierAPIBackend) GetRegisterNodeList(typ types.RegisteredNodeType) ([]*types.RegisteredNodeInfo, error) {
-	return nil, nil
+	return s.carrier.datachain.GetRegisterNodeList(typ)
 }
 
 func (s *CarrierAPIBackend) SendTaskEvent(event *event.TaskEvent) error  {
