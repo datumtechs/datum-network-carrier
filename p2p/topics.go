2ca524c61a9d4e18906e7e082ec4eb4f09419dcd
zhangjun <zhangjun@juzix.net>
2021-06-23 14:27:25 +0800

    infor/topics/interator/monitoring

---
+++ p2p/topics.go
@@ -0,0 +1,18 @@
+package p2p
+
+const (
+	//TODO: to define more topics....
+
+	// AttestationSubnetTopicFormat is the topic format for the attestation subnet.
+	AttestationSubnetTopicFormat = "/carrier/%x/beacon_attestation_%d"
+	// BlockSubnetTopicFormat is the topic format for the block subnet.
+	BlockSubnetTopicFormat = "/carrier/%x/carrier_block"
+	// ExitSubnetTopicFormat is the topic format for the voluntary exit subnet.
+	ExitSubnetTopicFormat = "/carrier/%x/voluntary_exit"
+	// ProposerSlashingSubnetTopicFormat is the topic format for the proposer slashing subnet.
+	ProposerSlashingSubnetTopicFormat = "/carrier/%x/proposer_slashing"
+	// AttesterSlashingSubnetTopicFormat is the topic format for the attester slashing subnet.
+	AttesterSlashingSubnetTopicFormat = "/carrier/%x/attester_slashing"
+	// AggregateAndProofSubnetTopicFormat is the topic format for the aggregate and proof subnet.
+	AggregateAndProofSubnetTopicFormat = "/carrier/%x/beacon_aggregate_and_proof"
+)
