035b6ca30beb7af70404f7f374251a831c51e4cf
zhangjun <zhangjun@juzix.net>
2021-06-24 17:10:29 +0800

    test for utils of p2p module

--- p2p/utils_test.go
+++ p2p/utils_test.go
@@ -1,1 +1,33 @@
 package p2p
+
+import (
+	"fmt"
+	"testing"
+)
+
+// Test `verifyConnectivity` function by trying to connect to google.com (successfully)
+// and then by connecting to an unreachable IP and ensuring that a log is emitted
+func TestVerifyConnectivity(t *testing.T) {
+	//hook := logTest.NewGlobal()
+	cases := []struct {
+		address              string
+		port                 uint
+		expectedConnectivity bool
+		name                 string
+	}{
+		{"142.250.68.46", 80, true, "Dialing a reachable IP: 142.250.68.46:80"}, // google.com
+		{"123.123.123.123", 19000, false, "Dialing an unreachable IP: 123.123.123.123:19000"},
+	}
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf(tc.name),
+			func(t *testing.T) {
+				verifyConnectivity(tc.address, tc.port, "tcp")
+				//logMessage := "IP address is not accessible"
+				if tc.expectedConnectivity {
+					//require.Contains(t, hook, logMessage)
+				} else {
+					//require.Contains(t, hook, logMessage)
+				}
+			})
+	}
+}
