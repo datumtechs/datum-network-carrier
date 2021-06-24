b66811be546a7e1c75e3a0c6a2dbceabd6161efb
zhangjun <zhangjun@juzix.net>
2021-06-24 11:58:18 +0800

    discovery

--- p2p/broadcaster.go
+++ p2p/broadcaster.go
@@ -1,1 +1,59 @@
 package p2p
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"github.com/RosettaFlow/Carrier-Go/params"
+	"github.com/gogo/protobuf/proto"
+	"github.com/pkg/errors"
+	"go.opencensus.io/trace"
+	"reflect"
+	"time"
+)
+
+// ErrMessageNotMapped occurs on a Broadcast attempt when a message has not been defined in the GossipTypeMapping.
+var ErrMessageNotMapped = errors.New("message type is not mapped to a PubSub topic")
+
+// Broadcast a message to the p2p network.
+func (s *Service) Broadcast(ctx context.Context, msg proto.Message) error {
+	ctx, span := trace.StartSpan(ctx, "p2p.Broadcast")
+	defer span.End()
+
+	twoSlots := time.Duration(2*params.CarrierChainConfig().SecondsPerSlot) * time.Second
+	ctx, cancel := context.WithTimeout(ctx, twoSlots)
+	defer cancel()
+
+	forkDigest, err := s.forkDigest()
+	if err != nil {
+		err := errors.Wrap(err, "could not retrieve fork digest")
+		//traceutil.AnnotateError(span, err)
+		return err
+	}
+
+	topic, ok := GossipTypeMapping[reflect.TypeOf(msg)]
+	if !ok {
+		//traceutil.AnnotateError(span, ErrMessageNotMapped)
+		return ErrMessageNotMapped
+	}
+	return s.broadcastObject(ctx, msg, fmt.Sprintf(topic, forkDigest))
+}
+
+// To broadcast message to other peers in our gossip mesh.
+func (s *Service) broadcastObject(ctx context.Context, obj interface{}, topic string) error {
+	buf := new(bytes.Buffer)
+	if _, err := s.Encoding().EncodeGossip(buf, obj); err != nil {
+		err := errors.Wrap(err, "could not encode message")
+		return err
+	}
+	if err := s.PublishToTopic(ctx, topic+s.Encoding().ProtocolSuffix(), buf.Bytes()); err != nil {
+		err := errors.Wrap(err, "could not publish message")
+		return err
+	}
+	return nil
+}
+
+func attestationToTopic(subnet uint64, forkDigest [4]byte) string {
+	return fmt.Sprintf(AttestationSubnetTopicFormat, forkDigest, subnet)
+}
+
--- p2p/fork.go
+++ p2p/fork.go
@@ -1,1 +1,68 @@
 package p2p
+
+import (
+	"github.com/RosettaFlow/Carrier-Go/params"
+	"github.com/ethereum/go-ethereum/p2p/enode"
+	"time"
+)
+
+// ENR key used for eth2-related fork data.
+var eth2ENRKey = params.CarrierNetworkConfig().ETH2Key
+
+// ForkDigest returns the current fork digest of
+// the node.
+func (s *Service) forkDigest() ([4]byte, error) {
+	//if s.currentForkDigest != [4]byte{} {
+	//	return s.currentForkDigest, nil
+	//}
+	//fd, err := p2putils.CreateForkDigest(s.genesisTime, s.genesisValidatorsRoot)
+	//if err != nil {
+	//	s.currentForkDigest = fd
+	//}
+	//return fd, err
+	return [4]byte{0x1, 0x1, 0x0, 0x0}, nil
+}
+
+// Adds a fork entry as an ENR record under the eth2EnrKey for
+// the local node. The fork entry is an ssz-encoded enrForkID type
+// which takes into account the current fork version from the current
+// epoch to create a fork digest, the next fork version,
+// and the next fork epoch.
+func addForkEntry(
+	node *enode.LocalNode,
+	genesisTime time.Time,
+	genesisValidatorsRoot []byte,
+) (*enode.LocalNode, error) {
+	//digest, err := p2putils.CreateForkDigest(genesisTime, genesisValidatorsRoot)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//currentSlot := helpers.SlotsSince(genesisTime)
+	//currentEpoch := helpers.SlotToEpoch(currentSlot)
+	//if timeutils.Now().Before(genesisTime) {
+	//	currentEpoch = 0
+	//}
+	//fork, err := p2putils.Fork(currentEpoch)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//
+	//nextForkEpoch := params.BeaconConfig().NextForkEpoch
+	//nextForkVersion := params.BeaconConfig().NextForkVersion
+	//// Set to the current fork version if our next fork is not planned.
+	//if nextForkEpoch == math.MaxUint64 {
+	//	nextForkVersion = fork.CurrentVersion
+	//}
+	//enrForkID := &pb.ENRForkID{
+	//	CurrentForkDigest: digest[:],
+	//	NextForkVersion:   nextForkVersion,
+	//	NextForkEpoch:     nextForkEpoch,
+	//}
+	//enc, err := enrForkID.MarshalSSZ()
+	//if err != nil {
+	//	return nil, err
+	//}
+	//forkEntry := enr.WithEntry(eth2ENRKey, enc)
+	//node.Set(forkEntry)
+	return node, nil
+}
