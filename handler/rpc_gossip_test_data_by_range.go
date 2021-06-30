2f453d7ea13466b9a4ad8c9f6657d6cf7f31848b
zjsunzone <zjsunzone@gmail.com>
2021-06-30 17:04:56 +0800

    update rpc_gossip_test_data_by_range

--- handler/rpc_gossip_test_data_by_range.go
+++ handler/rpc_gossip_test_data_by_range.go
@@ -2,15 +2,18 @@ package handler
 
 import (
 	"context"
-	pb "github.com/RosettaFlow/Carrier-Go/lib/p2p/v1"
+	libp2ppb "github.com/RosettaFlow/Carrier-Go/lib/rpc/v1"
+	p2ptypes "github.com/RosettaFlow/Carrier-Go/p2p/types"
 	libp2pcore "github.com/libp2p/go-libp2p-core"
 	"github.com/pkg/errors"
-	types "github.com/prysmaticlabs/eth2-types"
+	"github.com/prysmaticlabs/prysm/shared/traceutil"
+	"go.opencensus.io/trace"
 	"time"
 )
 
-// carrierBlocksByRangeRPCHandler looks up the request blocks from the database from a given start block.
-func (s *Service) carrierBlocksByRangeRPCHandler(ctx context.Context, msg interface{}, stream libp2pcore.Stream) error {
+func (s *Service) gossipTestDataByRangeRPCHandler(ctx context.Context, msg interface{}, stream libp2pcore.Stream) error {
+	ctx, span := trace.StartSpan(ctx, "handler.GossipTestDataByRangeRPCHandler")
+	defer span.End()
 	ctx, cancel := context.WithTimeout(ctx, respTimeout)
 	defer cancel()
 	SetRPCStreamDeadlines(stream)
@@ -19,27 +22,89 @@ func (s *Service) carrierBlocksByRangeRPCHandler(ctx context.Context, msg interf
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 
-	m, ok := msg.(*pb.CarrierBlocksByRangeRequest)
+	m, ok := msg.(*libp2ppb.GossipTestData)	// as request.
 	if !ok {
-		return errors.New("message is not type *pb.CarrierBlockByRangeRequest")
+		return errors.New("message is not type *p2ppb.GossipTestData")
 	}
-	if err := s.validateRangeRequest(m); err != nil {
+	if err := s.validateGossipRangeRequest(m); err != nil {
 		s.writeErrorResponseToStream(responseCodeInvalidRequest, err.Error(), stream)
 		s.cfg.P2P.Peers().Scorers().BadResponsesScorer().Increment(stream.Conn().RemotePeer())
+		traceutil.AnnotateError(span, err)
 		return err
 	}
-	//TODO: ....
+
+	err := s.writeGossipTestDataRangeToStream(ctx, stream)
+	if err != nil && !errors.Is(err, p2ptypes.ErrInvalidParent) {
+		return err
+	}
 	closeStream(stream, log)
 	return nil
 }
 
-func (s *Service) writeBlockRangeToStream(ctx context.Context, startSlot, endSlot types.Slot,
-	step uint64, stream libp2pcore.Stream) error {
-	//TODO: ....
+func (s *Service) writeGossipTestDataRangeToStream(ctx context.Context, stream libp2pcore.Stream) error {
+	ctx, span := trace.StartSpan(ctx, "sync.WriteGossipTestDataRangeToStream")
+	defer span.End()
+
+	blks, err := generateTestData()
+	if err != nil {
+		log.WithError(err).Debug("Could not get gossip test data")
+		s.writeErrorResponseToStream(responseCodeServerError, p2ptypes.ErrGeneric.Error(), stream)
+		traceutil.AnnotateError(span, err)
+		return err
+	}
+	for _, b := range blks {
+		if b == nil || b.Data == nil {
+			continue
+		}
+		if chunkErr := s.chunkWriter(stream, b); chunkErr != nil {
+			log.WithError(chunkErr).Debug("Could not send a chunked response")
+			s.writeErrorResponseToStream(responseCodeServerError, p2ptypes.ErrGeneric.Error(), stream)
+			traceutil.AnnotateError(span, chunkErr)
+			return chunkErr
+		}
+
+	}
+	// Return error in the event we have an invalid parent.
+	return err
+}
+
+func (s *Service) validateGossipRangeRequest(r *libp2ppb.GossipTestData) error {
+	count := r.Count
+	step := r.Step
+	if count < 10 {
+		return p2ptypes.ErrInvalidRequest
+	}
+	if step < 1 {
+		return p2ptypes.ErrInvalidRequest
+	}
 	return nil
 }
 
-func (s *Service) validateRangeRequest(r *pb.CarrierBlocksByRangeRequest) error {
-	//TODO: ...
-	return nil
+func generateTestData() ([]*libp2ppb.SignedGossipTestData, error) {
+	return []*libp2ppb.SignedGossipTestData{
+		{
+			Data:                 &libp2ppb.GossipTestData{
+				Data:                 []byte("data01"),
+				Count:                11,
+				Step:                 11,
+			},
+			Signature:            make([]byte, 48),
+		},
+		{
+			Data:                 &libp2ppb.GossipTestData{
+				Data:                 []byte("data02"),
+				Count:                22,
+				Step:                 22,
+			},
+			Signature:            make([]byte, 48),
+		},
+		{
+			Data:                 &libp2ppb.GossipTestData{
+				Data:                 []byte("data03"),
+				Count:                33,
+				Step:                 33,
+			},
+			Signature:            make([]byte, 48),
+		},
+	}, nil
 }
