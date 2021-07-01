dcc9eaa69038a95cae2a205fd73d79e29f1b5b17
zjsunzone <zjsunzone@gmail.com>
2021-07-01 17:08:51 +0800

    usage for debug

---
+++ cmd/carrier/usage.go
@@ -0,0 +1,27 @@
+package main
+
+
+var appHelpTemplate = `NAME:
+   {{.App.Name}} - {{.App.Usage}}
+USAGE:
+   {{.App.HelpName}} [options]{{if .App.Commands}} command [command options]{{end}} {{if .App.ArgsUsage}}{{.App.ArgsUsage}}{{else}}[arguments...]{{end}}
+   {{if .App.Version}}
+AUTHOR:
+   {{range .App.Authors}}{{ . }}{{end}}
+   {{end}}{{if .App.Commands}}
+GLOBAL OPTIONS:
+   {{range .App.Commands}}{{join .Names ", "}}{{ "\t" }}{{.Usage}}
+   {{end}}{{end}}{{if .FlagGroups}}
+{{range .FlagGroups}}{{.Name}} OPTIONS:
+   {{range .Flags}}{{.}}
+   {{end}}
+{{end}}{{end}}{{if .App.Copyright }}
+COPYRIGHT:
+   {{.App.Copyright}}
+VERSION:
+   {{.App.Version}}
+   {{end}}{{if len .App.Authors}}
+   {{end}}
+`
+
+
