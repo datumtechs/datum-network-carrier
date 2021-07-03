44d115d43e5aecd0a9aa599690996a50700c50dc
zhangjun <zhangjun@juzix.net>
2021-07-03 14:33:47 +0800

    add static node load

--- node/registration/p2p.go
+++ node/registration/p2p.go
@@ -1,12 +1,19 @@
 package registration
 
 import (
+	"github.com/RosettaFlow/Carrier-Go/common/fileutil"
 	"github.com/RosettaFlow/Carrier-Go/common/flags"
 	"github.com/RosettaFlow/Carrier-Go/params"
+	"github.com/prysmaticlabs/prysm/shared/sliceutil"
 	"github.com/urfave/cli/v2"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"os"
 	"path/filepath"
+)
+
+const (
+	datadirStaticNodes     = "static-nodes.json"  // Path within the datadir to the static node list
 )
 
 // P2PPreregistration prepares data for p2p.Service's registration.
@@ -51,4 +58,56 @@ func readbootNodes(fileName string) ([]string, error) {
 		return nil, err
 	}
 	return listNodes, nil
+}
+
+func readStaticNodesFromJSON(fileName string) ([]string, error) {
+	var nodeList []string
+	if err := fileutil.LoadJSON(fileName, &nodeList); err != nil {
+		//log.WithError(err).Errorf("Can't load sttaic node file from JSON %s: %v", fileName, err)
+		return nil, err
+	}
+	return nodeList, nil
+}
+
+func P2PStaticNodeAddrs(cliCtx *cli.Context, dataDir string) ([]string, error) {
+	// Static ENR may be a filepath to a YAML file
+	staticNodeAddrs := make([]string, 0)
+	cmdStatics := sliceutil.SplitCommaSeparated(cliCtx.StringSlice(flags.StaticPeers.Name))
+	if len(cmdStatics) != 0 {
+		staticNodeAddrs = append(staticNodeAddrs, cmdStatics...)
+	}
+	path := ResolvePath(dataDir, datadirStaticNodes)
+	if path == "" {
+		return staticNodeAddrs, nil
+	}
+	if _, err := os.Stat(path); err != nil {
+		return staticNodeAddrs, nil
+	}
+	if filepath.Ext(path) == ".yaml" {
+		fileNodes, err := readbootNodes(path)
+		if err != nil {
+			log.WithError(err).Infof("Can't load static node file from YML file %s: %v", path, err)
+			return staticNodeAddrs, nil
+		}
+		staticNodeAddrs = append(staticNodeAddrs, fileNodes...)
+	}
+	if filepath.Ext(path) == ".json" {
+		fileNodes, err := readStaticNodesFromJSON(path)
+		if err != nil {
+			log.WithError(err).Errorf("Can't load sttaic node file from JSON %s: %v", path, err)
+			return staticNodeAddrs, nil
+		}
+		staticNodeAddrs = append(staticNodeAddrs, fileNodes...)
+	}
+	return staticNodeAddrs, nil
+}
+
+func ResolvePath(dataDir, path string) string {
+	if filepath.IsAbs(path) {
+		return path
+	}
+	if dataDir == "" {
+		return ""
+	}
+	return filepath.Join(dataDir, path)
 }
