91f1fbc3e52ec78c425065c87dc5b0e9f5557995
zjsunzone <zjsunzone@gmail.com>
2021-07-07 10:54:19 +0800

    to split files

---
+++ rpc/backend/power_service.go
@@ -0,0 +1,126 @@
+package backend
+
+import (
+	"context"
+	pb "github.com/RosettaFlow/Carrier-Go/lib/api"
+	"github.com/RosettaFlow/Carrier-Go/types"
+	"time"
+)
+
+//
+//func (svr *PowerServiceServer) GetPowerTotalSummaryList(ctx context.Context, req *pb.EmptyGetParams) (*pb.GetPowerTotalSummaryListResponse, error) {
+//	return nil, nil
+//}
+//func (svr *PowerServiceServer) GetPowerSingleSummaryList(ctx context.Context, req *pb.EmptyGetParams) (*pb.GetPowerSingleSummaryListResponse, error) {
+//	return nil, nil
+//}
+//func (svr *PowerServiceServer) GetPowerTotalSummaryByState(ctx context.Context, req *pb.GetPowerTotalSummaryByStateRequest) (*pb.GetPowerTotalSummaryListResponse, error) {
+//	return nil, nil
+//}
+//func (svr *PowerServiceServer) GetPowerSingleSummaryByState(ctx context.Context, req *pb.GetPowerSingleSummaryByStateRequest) (*pb.GetPowerSingleSummaryListResponse, error) {
+//	return nil, nil
+//}
+//func (svr *PowerServiceServer) GetPowerTotalSummaryByOwner(ctx context.Context, req *pb.GetPowerTotalSummaryByOwnerRequest) (*pb.GetPowerTotalSummaryResponse, error) {
+//	return nil, nil
+//}
+//func (svr *PowerServiceServer) GetPowerSingleSummaryByOwner(ctx context.Context, req *pb.GetPowerSingleSummaryByOwnerRequest) (*pb.GetPowerSingleSummaryListResponse, error) {
+//	return nil, nil
+//}
+//func (svr *PowerServiceServer) GetPowerSingleDetail(ctx context.Context, req *pb.GetPowerSingleDetailRequest) (*pb.GetPowerSingleDetailResponse, error) {
+//	return nil, nil
+//}
+func (svr *PowerServiceServer) GetPowerTotalDetailList(ctx context.Context, req *pb.EmptyGetParams) (*pb.GetPowerTotalDetailListResponse, error) {
+
+	powerList, err := svr.B.GetPowerTotalDetailList()
+	if nil != err {
+		return nil, NewRpcBizErr(ErrGetTotalPowerListStr)
+	}
+	respList := make([]*pb.GetPowerTotalDetailResponse, len(powerList))
+
+	for i, power := range powerList {
+
+		resp := &pb.GetPowerTotalDetailResponse{
+			Owner: types.ConvertNodeAliasToPB(power.Owner),
+			Power: &pb.PowerTotalDetail{
+				Information: types.ConvertResourceUsageToPB(power.PowerDetail.ResourceUsage),
+				TotalTaskCount:power.PowerDetail.TotalTaskCount, // TODO 管理台查询 全网算力， 暂不显示 任务实况
+				CurrentTaskCount: power.PowerDetail.CurrentTaskCount, // TODO 管理台查询 全网算力， 暂不显示 任务实况
+				Tasks: types.ConvertPowerTaskArrToPB(power.PowerDetail.Tasks), // TODO 管理台查询 全网算力， 暂不显示 任务实况
+				State: power.PowerDetail.State,
+			},
+		}
+		respList[i] = resp
+	}
+	return &pb.GetPowerTotalDetailListResponse{
+		PowerList: respList,
+	}, nil
+}
+func (svr *PowerServiceServer) GetPowerSingleDetailList(ctx context.Context, req *pb.EmptyGetParams) (*pb.GetPowerSingleDetailListResponse, error) {
+
+	powerList, err := svr.B.GetPowerSingleDetailList()
+	if nil != err {
+		return nil, NewRpcBizErr(ErrGetSinglePowerListStr)
+	}
+	respList := make([]*pb.GetPowerSingleDetailResponse, len(powerList))
+
+	for i, power := range powerList {
+
+		resp := &pb.GetPowerSingleDetailResponse{
+			Owner:  types.ConvertNodeAliasToPB(power.Owner),
+			Power: &pb.PowerSingleDetail{
+				JobNodeId: power.PowerDetail.JobNodeId,
+				PowerId: power.PowerDetail.PowerId,
+				Information:types.ConvertResourceUsageToPB(power.PowerDetail.ResourceUsage),
+				TotalTaskCount:power.PowerDetail.TotalTaskCount,
+				CurrentTaskCount: power.PowerDetail.CurrentTaskCount,
+				Tasks: types.ConvertPowerTaskArrToPB(power.PowerDetail.Tasks),
+				State: power.PowerDetail.State,
+			},
+		}
+		respList[i] = resp
+	}
+	return &pb.GetPowerSingleDetailListResponse{
+		PowerList: respList,
+	}, nil
+}
+func (svr *PowerServiceServer) PublishPower(ctx context.Context, req *pb.PublishPowerRequest) (*pb.PublishPowerResponse, error) {
+
+	powerMsg := new(types.PowerMsg)
+	powerMsg.Data.JobNodeId = req.JobNodeId
+	powerMsg.Data.CreateAt = uint64(time.Now().UnixNano())
+	powerMsg.Data.Name = req.Owner.Name
+	powerMsg.Data.NodeId = req.Owner.NodeId
+	powerMsg.Data.IdentityId = req.Owner.IdentityId
+	powerMsg.Data.Information.Processor = req.Information.Processor
+	powerMsg.Data.Information.Mem = req.Information.Mem
+	powerMsg.Data.Information.Bandwidth = req.Information.Bandwidth
+	powerId := powerMsg.GetPowerId()
+
+	err := svr.B.SendMsg(powerMsg)
+	if nil != err {
+		return nil, NewRpcBizErr(ErrSendPowerMsgStr)
+	}
+	return &pb.PublishPowerResponse{
+		Status:  0,
+		Msg:     OK,
+		PowerId: powerId,
+	}, nil
+}
+
+func (svr *PowerServiceServer) RevokePower(ctx context.Context, req *pb.RevokePowerRequest) (*pb.SimpleResponseCode, error) {
+	powerRevokeMsg := new(types.PowerRevokeMsg)
+	powerRevokeMsg.CreateAt = uint64(time.Now().UnixNano())
+	powerRevokeMsg.Name = req.Owner.Name
+	powerRevokeMsg.NodeId = req.Owner.NodeId
+	powerRevokeMsg.IdentityId = req.Owner.IdentityId
+	powerRevokeMsg.PowerId = req.PowerId
+
+	err := svr.B.SendMsg(powerRevokeMsg)
+	if nil != err {
+		return nil, NewRpcBizErr(ErrSendPowerRevokeMsgStr)
+	}
+	return &pb.SimpleResponseCode{
+		Status: 0,
+		Msg:    OK,
+	}, nil
+}
---
+++ rpc/backend/task_service.go
@@ -0,0 +1,124 @@
+package backend
+
+import (
+	"context"
+	pb "github.com/RosettaFlow/Carrier-Go/lib/api"
+	"github.com/RosettaFlow/Carrier-Go/types"
+	"time"
+)
+
+//func (svr *TaskServiceServer) GetTaskSummaryList(ctx context.Context, req *pb.EmptyGetParams) (*pb.GetTaskSummaryListResponse, error) {
+//	return nil, nil
+//}
+//func (svr *TaskServiceServer) GetTaskJoinSummaryList(ctx context.Context, req *pb.GetTaskJoinSummaryListRequest) (*pb.GetTaskJoinSummaryListResponse, error) {
+//	return nil, nil
+//}
+//func (svr *TaskServiceServer) GetTaskDetail(ctx context.Context, req *pb.GetTaskDetailRequest) (*pb.GetTaskDetailResponse, error) {
+//	return nil, nil
+//}
+func (svr *TaskServiceServer) GetTaskDetailList(ctx context.Context, req *pb.EmptyGetParams) (*pb.GetTaskDetailListResponse, error) {
+	tasks, err := svr.B.GetTaskDetailList()
+	if nil != err {
+		return nil, NewRpcBizErr(ErrGetNodeTaskListStr)
+	}
+	arr := make([]*pb.GetTaskDetailResponse, len(tasks))
+	for i, task := range tasks {
+		t := &pb.GetTaskDetailResponse{
+			Information: types.ConvertTaskDetailShowToPB(task),
+		}
+		arr[i] = t
+	}
+	return &pb.GetTaskDetailListResponse{
+		Status:   0,
+		Msg:      OK,
+		TaskList: arr,
+	}, nil
+}
+
+func (svr *TaskServiceServer) GetTaskEventList(ctx context.Context, req *pb.GetTaskEventListRequest) (*pb.GetTaskEventListResponse, error) {
+
+	events, err := svr.B.GetTaskEventList(req.TaskId)
+	if nil != err {
+		return nil, NewRpcBizErr(ErrGetNodeTaskEventListStr)
+	}
+
+	return &pb.GetTaskEventListResponse{
+		Status:        0,
+		Msg:           OK,
+		TaskEventList: types.ConvertTaskEventArrToPB(events),
+	}, nil
+}
+
+func (svr *TaskServiceServer) PublishTaskDeclare(ctx context.Context, req *pb.PublishTaskDeclareRequest) (*pb.PublishTaskDeclareResponse, error) {
+	taskMsg := new(types.TaskMsg)
+	taskMsg.Data.TaskName = req.TaskName
+	taskMsg.Data.CreateAt = uint64(time.Now().UnixNano())
+	taskMsg.Data.Owner.Name = req.Owner.MemberInfo.Name
+	taskMsg.Data.Owner.NodeId = req.Owner.MemberInfo.NodeId
+	taskMsg.Data.Owner.IdentityId = req.Owner.MemberInfo.IdentityId
+	taskMsg.Data.Owner.MetaData.ColumnIndexList = req.Owner.MetaDataInfo.ColumnIndexList
+	taskMsg.Data.Owner.MetaData.MetaDataId = req.Owner.MetaDataInfo.MetaDataId
+
+	partners := make([]*types.TaskSupplier, len(req.Partners))
+	for i, v := range req.Partners {
+		partner := &types.TaskSupplier{
+			NodeAlias: &types.NodeAlias{
+				Name:       v.MemberInfo.Name,
+				NodeId:     v.MemberInfo.NodeId,
+				IdentityId: v.MemberInfo.IdentityId,
+			},
+			MetaData: &types.SupplierMetaData{
+				MetaDataId:      v.MetaDataInfo.MetaDataId,
+				ColumnIndexList: v.MetaDataInfo.ColumnIndexList,
+			},
+		}
+		partners[i] = partner
+	}
+	taskMsg.Data.Partners = partners
+
+	receivers := make([]*types.TaskResultReceiver, len(req.Receivers))
+	for i, v := range req.Receivers {
+
+		providers := make([]*types.NodeAlias, len(v.Providers))
+		for j, val := range v.Providers {
+			provider := &types.NodeAlias{
+				Name:       val.Name,
+				NodeId:     val.NodeId,
+				IdentityId: val.IdentityId,
+			}
+			providers[j] = provider
+		}
+
+		receiver := &types.TaskResultReceiver{
+			NodeAlias: &types.NodeAlias{
+				Name:       v.MemberInfo.Name,
+				NodeId:     v.MemberInfo.NodeId,
+				IdentityId: v.MemberInfo.IdentityId,
+			},
+			Providers: providers,
+		}
+
+		receivers[i] = receiver
+	}
+	taskMsg.Data.Receivers = receivers
+
+	taskMsg.Data.CalculateContractCode = req.CalculateContractcode
+	taskMsg.Data.DataSplitContractCode = req.DatasplitContractcode
+	taskMsg.Data.OperationCost = &types.TaskOperationCost{
+		Processor: req.OperationCost.CostProcessor,
+		Mem:       req.OperationCost.CostMem,
+		Bandwidth: req.OperationCost.CostBandwidth,
+		Duration:  req.OperationCost.Duration,
+	}
+	taskId := taskMsg.GetTaskId()
+
+	err := svr.B.SendMsg(taskMsg)
+	if nil != err {
+		return nil, NewRpcBizErr(ErrSendTaskMsgStr)
+	}
+	return &pb.PublishTaskDeclareResponse{
+		Status: 0,
+		Msg:    OK,
+		TaskId: taskId,
+	}, nil
+}
