69e9e8528332d1be8b0e502b36aa3da4cceb5a39
zhangjun <zhangjun@juzix.net>
2021-07-14 18:19:17 +0800

    update format

--- rpc/backend/metadata/cms_service.go
+++ rpc/backend/metadata/cms_service.go
@@ -73,27 +73,6 @@ func (svr *MetaDataServiceServer) GetMetaDataDetailList(ctx context.Context, req
 	}, nil
 }
 
-func (svr *MetaDataServiceServer) GetMetaDataDetailListByOwner(ctx context.Context, req *pb.GetMetaDataDetailListByOwnerRequest) (*pb.GetMetaDataDetailListResponse, error) {
-	metaDataList, err := svr.B.GetMetaDataDetailListByOwner(req.IdentityId)
-	if nil != err {
-		return nil, backend.NewRpcBizErr(ErrGetMetaDataDetailListStr)
-	}
-	respList := make([]*pb.GetMetaDataDetailResponse, len(metaDataList))
-	for i, metaDataDetail := range metaDataList {
-		resp := &pb.GetMetaDataDetailResponse{
-			Owner:       types.ConvertNodeAliasToPB(metaDataDetail.Owner),
-			Information: types.ConvertMetaDataInfoToPB(metaDataDetail.MetaData),
-		}
-		respList[i] = resp
-	}
-
-	return &pb.GetMetaDataDetailListResponse{
-		Status:       0,
-		Msg:          backend.OK,
-		MetaDataList: respList,
-	}, nil
-}
-
 func (svr *MetaDataServiceServer) PublishMetaData(ctx context.Context, req *pb.PublishMetaDataRequest) (*pb.PublishMetaDataResponse, error) {
 	if req == nil || req.Owner == nil {
 		return nil, errors.New("required owner")
