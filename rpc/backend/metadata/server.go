b7d17a968b458422533c2233411f661e19a50f1f
zhangjun <zhangjun@juzix.net>
2021-07-23 12:08:24 +0800

    redefine some errors

--- rpc/backend/metadata/server.go
+++ rpc/backend/metadata/server.go
@@ -6,7 +6,14 @@ const (
 	ErrSendMetaDataRevokeMsgStr = "Failed to send metaDataRevokeMsg"
 	ErrGetMetaDataDetailStr     = "Failed to get metadata detail"
 	ErrGetMetaDataDetailListStr = "Failed to get metadata detail list"
-	ErrSendMetaDataMsgStr    = "Failed to send metaDataMsg"
+	ErrSendMetaDataMsgStr       = "Failed to send metaDataMsg"
+)
+
+var (
+	ErrSendMetaDataRevokeMessage = &backend.RpcBizErr{Msg: "Failed to send metaDataRevokeMsg"}
+	ErrGetMetaDataDetail         = &backend.RpcBizErr{Msg: "Failed to get metadata detail"}
+	ErrSendIdentityMessage       = &backend.RpcBizErr{Msg: "Failed to get metadata detail list"}
+	ErrGetMetaDataDetailList     = &backend.RpcBizErr{Msg: "Failed to send metaDataMsg"}
 )
 
 type MetaDataServiceServer struct {
--- rpc/backend/power/server.go
+++ rpc/backend/power/server.go
@@ -9,6 +9,13 @@ const (
 	ErrSendPowerMsgStr       = "Failed to send powerMsg"
 )
 
+var (
+	ErrSendPowerRevokeMessage = &backend.RpcBizErr{Msg: "Failed to send powerRevokeMsg"}
+	ErrGetTotalPowerList      = &backend.RpcBizErr{Msg: "Failed to get total power list"}
+	ErrGetSinglePowerList     = &backend.RpcBizErr{Msg: "Failed to get current node power list"}
+	ErrSendPowerMsg           = &backend.RpcBizErr{Msg: "Failed to send powerMsg"}
+)
+
 type PowerServiceServer struct {
 	B backend.Backend
 }
