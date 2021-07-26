986cba8891ff6a10ca8a923d8c21457487b2c338
zhangjun <zhangjun@juzix.net>
2021-07-26 15:55:58 +0800

    keytool command be added

---
+++ cmd/keytool/genkeypair.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"encoding/hex"
+	"fmt"
+	"github.com/RosettaFlow/Carrier-Go/cmd/utils"
+	gcrypto "github.com/ethereum/go-ethereum/crypto"
+	"github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/urfave/cli/v2"
+)
+
+type outputGenkeypair struct {
+	PrivateKey string
+	PublicKey  string
+	PeerId     string
+}
+
+var commandGenkeypair = &cli.Command{
+	Name:      "genkeypair",
+	Usage:     "generate new private key pair",
+	ArgsUsage: "[ ]",
+	Description: `
+Generate a new private key pair.
+.
+`,
+	Flags: []cli.Flag{
+		jsonFlag,
+	},
+	Action: func(ctx *cli.Context) error {
+		// Check if keyfile path given and make sure it doesn't already exist.
+		var privateKey *ecdsa.PrivateKey
+		var err error
+		// generate random.
+		privateKey, err = gcrypto.GenerateKey()
+		if err != nil {
+			utils.Fatalf("Failed to generate random private key: %v", err)
+		}
+
+		typeAssertedKey := crypto.PubKey((*crypto.Secp256k1PublicKey)(&privateKey.PublicKey))
+		id, err := peer.IDFromPublicKey(typeAssertedKey)
+
+		// Output some information.
+		out := outputGenkeypair{
+			PublicKey:  hex.EncodeToString(gcrypto.FromECDSAPub(&privateKey.PublicKey)[1:]),
+			PrivateKey: hex.EncodeToString(gcrypto.FromECDSA(privateKey)),
+			PeerId:     id.String(),
+		}
+		if ctx.Bool(jsonFlag.Name) {
+			mustPrintJSON(out)
+		} else {
+			fmt.Println("PrivateKey: ", out.PrivateKey)
+			fmt.Println("PublicKey : ", out.PublicKey)
+			fmt.Println("PeerId : ", out.PeerId)
+		}
+		return nil
+	},
+}
