b513c86336d3bae5cdd8d217be4c9daa3c506703
GavinXu <meiyan532484710@163.com>
2021-08-07 12:13:59 +0800

    修复task 缺失 startAt 字段 Signed-off-by: GavinXu <meiyan532484710@163.com>

--- types/extend_types.go
+++ types/extend_types.go
@@ -27,6 +27,7 @@ func NewTaskDetailShowFromTaskData(input *Task, role string) *TaskDetailShow {
 		PowerSupplier: make([]*TaskPowerSupplierShow, 0, len(taskData.GetResourceSupplier())),
 		Receivers:     make([]*TaskNodeAlias, 0, len(taskData.GetReceivers())),
 		CreateAt:      taskData.GetCreateAt(),
+		StartAt:       taskData.GetStartAt(),
 		EndAt:         taskData.GetEndAt(),
 		State:         taskData.GetState(),
 		OperationCost: &TaskOperationCost{
@@ -80,85 +81,6 @@ func NewTaskDetailShowFromTaskData(input *Task, role string) *TaskDetailShow {
 	}
 	return detailShow
 }
-
-//func NewTaskDetailShowArrayFromTaskDataArray(input TaskDataArray) []*TaskDetailShow {
-//	taskDetailShowArray := make([]*TaskDetailShow, 0, input.Len())
-//	for _, task := range input {
-//		taskData := task.TaskData()
-//		detailShow := &TaskDetailShow{
-//			TaskId:        taskData.GetTaskId(),
-//			TaskName:      taskData.GetTaskName(),
-//			Owner:         &TaskNodeAlias{
-//				PartyId:    taskData.GetPartyId(),
-//				Name:       taskData.GetNodeName(),
-//				NodeId:     taskData.GetNodeId(),
-//				IdentityId: taskData.GetIdentity(),
-//			},
-//			AlgoSupplier:  &TaskNodeAlias{
-//				PartyId: 	taskData.GetPartyId(),
-//				Name:       taskData.GetNodeName(),
-//				NodeId:     taskData.GetNodeId(),
-//				IdentityId: taskData.GetIdentity(),
-//			},
-//			DataSupplier:  make([]*TaskDataSupplierShow, 0, len(taskData.GetMetadataSupplier())),
-//			PowerSupplier: make([]*TaskPowerSupplierShow, 0, len(taskData.GetResourceSupplier())),
-//			Receivers:     make([]*TaskNodeAlias, 0, len(taskData.GetReceivers())),
-//			CreateAt:      taskData.GetCreateAt(),
-//			EndAt:         taskData.GetEndAt(),
-//			State:         taskData.GetState(),
-//			OperationCost: &TaskOperationCost{
-//				Processor: uint64(taskData.GetTaskResource().GetCostProcessor()),
-//				Mem:       taskData.GetTaskResource().GetCostMem(),
-//				Bandwidth: taskData.GetTaskResource().GetCostBandwidth(),
-//				Duration:  taskData.GetTaskResource().GetDuration(),
-//			},
-//		}
-//		// DataSupplier
-//		for _, metadataSupplier := range taskData.GetMetadataSupplier() {
-//			dataSupplier := &TaskDataSupplierShow{
-//				MemberInfo: &TaskNodeAlias{
-//					PartyId:    metadataSupplier.GetOrganization().GetPartyId(),
-//					Name:       metadataSupplier.GetOrganization().GetNodeName(),
-//					NodeId:     metadataSupplier.GetOrganization().GetNodeId(),
-//					IdentityId: metadataSupplier.GetOrganization().GetIdentity(),
-//				},
-//				MetaDataId:     metadataSupplier.GetMetaId(),
-//				MetaDataName:   metadataSupplier.GetMetaName(),
-//			}
-//			detailShow.DataSupplier = append(detailShow.DataSupplier, dataSupplier)
-//		}
-//		// powerSupplier
-//		for _, data := range taskData.GetResourceSupplier() {
-//			detailShow.PowerSupplier = append(detailShow.PowerSupplier, &TaskPowerSupplierShow{
-//				MemberInfo:    &TaskNodeAlias{
-//					PartyId:    data.GetOrganization().GetPartyId(),
-//					Name:       data.GetOrganization().GetNodeName(),
-//					NodeId:     data.GetOrganization().GetNodeId(),
-//					IdentityId: data.GetOrganization().GetIdentity(),
-//				},
-//				ResourceUsage: &ResourceUsage{
-//					TotalMem:       data.GetResourceUsedOverview().GetTotalMem(),
-//					UsedMem:        data.GetResourceUsedOverview().GetUsedMem(),
-//					TotalProcessor: uint64(data.GetResourceUsedOverview().GetTotalProcessor()),
-//					UsedProcessor:  uint64(data.GetResourceUsedOverview().GetUsedProcessor()),
-//					TotalBandwidth: data.GetResourceUsedOverview().GetTotalBandwidth(),
-//					UsedBandwidth:  data.GetResourceUsedOverview().GetUsedBandwidth(),
-//				},
-//			})
-//		}
-//		// Receivers
-//		for _, receiver := range taskData.GetReceivers() {
-//			detailShow.Receivers = append(detailShow.Receivers, &TaskNodeAlias{
-//				PartyId:    receiver.GetReceiver().GetPartyId(),
-//				Name:       receiver.GetReceiver().GetNodeName(),
-//				NodeId:     receiver.GetReceiver().GetNodeId(),
-//				IdentityId: receiver.GetReceiver().GetIdentity(),
-//			})
-//		}
-//		taskDetailShowArray = append(taskDetailShowArray, detailShow)
-//	}
-//	return taskDetailShowArray
-//}
 
 func NewTaskEventFromAPIEvent(input []*api.TaskEvent) []*TaskEvent {
 	result := make([]*TaskEvent, 0, len(input))
