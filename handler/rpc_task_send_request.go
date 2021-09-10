de2c0e9095ccc3aa2619ebb172897695277f3dcf
GavinXu <meiyan532484710@163.com>
2021-09-10 18:15:39 +0800

    解决 循环引用 Signed-off-by: GavinXu <meiyan532484710@163.com>

---
+++ handler/rpc_task_send_request.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"context"
+	"errors"
+	pb "github.com/RosettaFlow/Carrier-Go/lib/consensus/twopc"
+	"github.com/RosettaFlow/Carrier-Go/p2p"
+	"github.com/libp2p/go-libp2p-core/peer"
+)
+
+// SendTaskResultMsg sends taskResult to other peer, if the task has finished.
+func SendTaskResultMsg(ctx context.Context, p2pProvider p2p.P2P, pid peer.ID, req *pb.TaskResultMsg) error {
+	ctx, cancel := context.WithTimeout(ctx, respTimeout)
+	defer cancel()
+
+	// send request on the special topic.
+	stream, err := p2pProvider.Send(ctx, req, p2p.RPCTaskResultMsgTopic, pid)
+	if err != nil {
+		return err
+	}
+	defer closeStream(stream, log)
+	code, errMsg, err := ReadStatusCode(stream, p2pProvider.Encoding())
+	if err != nil {
+		return err
+	}
+	if code != 0 {
+		return errors.New(errMsg)
+	}
+	return nil
+}
