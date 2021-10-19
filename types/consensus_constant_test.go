5d0f3577d7e99ca0ad5c398a5d910a6a510dc679
zjsunzone <zjsunzone@gmail.com>
2021-10-19 17:23:17 +0800

    add:TestTwopcMsgOption

--- types/consensus_constant_test.go
+++ types/consensus_constant_test.go
@@ -1,1 +1,23 @@
 package types
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestTwopcMsgOption(t *testing.T) {
+	start := TwopcMsgStart
+	unknown := TwopcMsgUnknown
+	stop := TwopcMsgStop
+	require.Equal(t, start.String(), "Start")
+	require.Equal(t, stop.String(), "Stop")
+	require.Equal(t, unknown.String(), "Unknown")
+
+	require.Equal(t, TwopcMsgOptionFromUint8(0), unknown)
+	require.Equal(t, TwopcMsgOptionFromUint8(1), start)
+	require.Equal(t, TwopcMsgOptionFromUint8(2), stop)
+
+	require.Equal(t, TwopcMsgOptionFromBytes([]byte{0}), unknown)
+	require.Equal(t, TwopcMsgOptionFromBytes([]byte{1}), start)
+	require.Equal(t, TwopcMsgOptionFromBytes([]byte{2}), stop)
+}
