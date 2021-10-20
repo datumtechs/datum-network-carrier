411fcda59e464ba20f3f65b3d13da969c190c0ee
zjsunzone <zjsunzone@gmail.com>
2021-10-20 14:57:08 +0800

    unit test:TestService_RejectInboundPeersBeyondLimit

--- p2p/connection_gater_test.go
+++ p2p/connection_gater_test.go
@@ -4,11 +4,14 @@ import (
 	"context"
 	"fmt"
 	"github.com/RosettaFlow/Carrier-Go/p2p/peers"
+	"github.com/RosettaFlow/Carrier-Go/p2p/peers/peerdata"
 	"github.com/RosettaFlow/Carrier-Go/p2p/peers/scorers"
+	p2ptest "github.com/RosettaFlow/Carrier-Go/p2p/testing"
 	"github.com/kevinms/leakybucket-go"
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/peer"
 	ma "github.com/multiformats/go-multiaddr"
+	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -88,4 +91,59 @@ func TestService_InterceptBannedIP(t *testing.T) {
 	if valid {
 		t.Errorf("Expected multiaddress with ip %s to be rejected as it exceeds the burst limit", ip)
 	}
+}
+
+func TestService_RejectInboundPeersBeyondLimit(t *testing.T) {
+	limit := 20
+	s := &Service{
+		ipLimiter: leakybucket.NewCollector(ipLimit, ipBurst, false),
+		peers: peers.NewStatus(context.Background(), &peers.StatusConfig{
+			PeerLimit:    limit,
+			ScorerParams: &scorers.Config{},
+		}),
+		host: p2ptest.NewTestP2P(t).BHost,
+		cfg:  &Config{MaxPeers: uint(limit)},
+	}
+	var err error
+	s.addrFilter, err = configureFilter(&Config{})
+	require.NoError(t, err)
+	ip := "192.168.1.111"
+	multiAddress, err := ma.NewMultiaddr(fmt.Sprintf("/ip4/%s/tcp/%d", ip, 3000))
+	require.NoError(t, err)
+
+	valid := s.InterceptAccept(&maEndpoints{raddr: multiAddress})
+	if !valid {
+		t.Errorf("Expected multiaddress with ip %s to be accepted as it is below the inbound limit", ip)
+	}
+
+	inboundLimit := float64(limit) * peers.InboundRatio
+	inboundLimit += highWatermarkBuffer
+	// top off by 1 to trigger it above the limit.
+	inboundLimit += 1
+	// Add in up to inbound peer limit.
+	for i := 0; i < int(inboundLimit); i++ {
+		addPeer(t, s.peers, peerdata.PeerConnectionState(ethpb.ConnectionState_CONNECTED))
+	}
+	valid = s.InterceptAccept(&maEndpoints{raddr: multiAddress})
+	if valid {
+		t.Errorf("Expected multiaddress with ip %s to be rejected as it exceeds the inbound limit", ip)
+	}
+}
+
+// Mock type for testing.
+type maEndpoints struct {
+	laddr ma.Multiaddr
+	raddr ma.Multiaddr
+}
+
+// LocalMultiaddr returns the local address associated with
+// this connection
+func (c *maEndpoints) LocalMultiaddr() ma.Multiaddr {
+	return c.laddr
+}
+
+// RemoteMultiaddr returns the remote address associated with
+// this connection
+func (c *maEndpoints) RemoteMultiaddr() ma.Multiaddr {
+	return c.raddr
 }
