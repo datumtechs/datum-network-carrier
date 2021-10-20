ea4408fdf3cb3426394588ef623ec5f33314c5f7
zjsunzone <zjsunzone@gmail.com>
2021-10-20 11:47:45 +0800

    test:TestMakePeer_OK

--- p2p/dial_relay_node_test.go
+++ p2p/dial_relay_node_test.go
@@ -1,6 +1,7 @@
 package p2p
 
 import (
+	"github.com/stretchr/testify/require"
 	"gotest.tools/assert"
 	"testing"
 )
@@ -8,4 +9,10 @@ import (
 func TestMakePeer_InvalidMultiaddress(t *testing.T) {
 	_, err := MakePeer("/ip4")
 	assert.ErrorContains(t, err, "failed to parse multiaddr \"/ip4\"",  "Expect error when invalid multiaddress was provided")
+}
+
+func TestMakePeer_OK(t *testing.T) {
+	a, err := MakePeer("/ip4/127.0.0.1/tcp/3333/p2p/QmUn6ycS8Fu6L462uZvuEfDoSgYX6kqP4aSZWMa7z1tWAX")
+	require.NoError(t, err, "Unexpected error when making a valid peer")
+	assert.Equal(t, "QmUn6ycS8Fu6L462uZvuEfDoSgYX6kqP4aSZWMa7z1tWAX", a.ID.Pretty(), "Unexpected peer ID")
 }
