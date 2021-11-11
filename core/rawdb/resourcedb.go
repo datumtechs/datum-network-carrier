80b2921da3a8a3db36a206e5cf49adc3f8184d57
Gavin520 <meiyan532484710@163.com>
2021-11-11 10:57:36 +0800

    Signed-off-by: Gavin520 <meiyan532484710@163.com>

--- core/rawdb/resourcedb.go
+++ core/rawdb/resourcedb.go
@@ -397,7 +397,7 @@ func QueryJobNodeRunningTaskIdCount(db KeyValueStore, jobNodeId string) (uint32,
 			}
 		}
 	}
-	log.Debugf("Call QueryJobNodeRunningTaskCount, jobNodeId: {%s}, taskIds count: %s", jobNodeId, count)
+	log.Debugf("Call QueryJobNodeRunningTaskCount, jobNodeId: {%s}, taskIds count: %d", jobNodeId, count)
 	return count, nil
 }
 
