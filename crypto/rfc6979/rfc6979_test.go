4b50063b7484e07a075d557ab109289b452ba73f
Gavin520 <meiyan532484710@163.com>
2021-12-01 11:25:44 +0800

    Signed-off-by: Gavin520 <meiyan532484710@163.com>

--- crypto/rfc6979/rfc6979_test.go
+++ crypto/rfc6979/rfc6979_test.go
@@ -1,19 +1,3 @@
-// Copyright 2018-2020 The PlatON Network Authors
-// This file is part of the PlatON-Go library.
-//
-// The PlatON-Go library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The PlatON-Go library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the PlatON-Go library. If not, see <http://www.gnu.org/licenses/>.
-
 package rfc6979
 
 import (
@@ -22,7 +6,7 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
-	"github.com/PlatONnetwork/PlatON-Go/crypto/secp256k1"
+	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 	"math/big"
 	"testing"
 )
