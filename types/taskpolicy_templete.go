069d0eacd23ae7fb80ab25202a8a87b46a4770bf
Gavin520 <meiyan532484710@163.com>
2022-03-30 17:35:29 +0800

    Signed-off-by: Gavin520 <meiyan532484710@163.com>

--- types/taskpolicy_templete.go
+++ types/taskpolicy_templete.go
@@ -11,28 +11,58 @@ var (
 	NotFoundPowerPolicy = fmt.Errorf("not found power policy")
 )
 
-//type TaskMetadataPolicy interface {
-//	GetPartyId() string
-//	GetMetadataId() string
-//	GetMetadataName() string
-//}
-
 const (
-	// data policy
+	// ==================================================================== metadata policy option ====================================================================
+	/**
+	[
+		{
+	        "partyId": "p0",
+	        "metadataId": "metadata:0xf7396b9a6be9ca6cd2099f0d5e67a748643898f2735573c0da44fc54880cf52d",
+			"metadataName" "aaa",
+	        "keyColumn": 1,
+	        "selectedColumns": [1, 2, 3]
+	    },
+		{
+	        "partyId": "p1",
+	        "metadataId": "metadata:0x2843e8103c065268991943c56d970570bedc8b31066e05ef7d62ea20b6c537c7",
+			"metadataName" "bbb",
+	        "keyColumn": 1,
+	        "selectedColumns": [1, 2, 3]
+	    }
+	]
+	 */
 	TASK_METADATA_POLICY_ROW_COLUMN = 1
 
-	// power policy
+	// ==================================================================== power policy option ====================================================================
+	/**
+	["q0", "q1", "q2"]
+	*/
 	TASK_POWER_POLICY_ASSIGNMENT_SYMBOL_RANDOM_ELECTION_POWER = 1
-	TASK_POWER_POLICY_DATANODE_PROVIDE_POWER                  = 2
+	/**
+	[{"p0", "identityId:0x...a"}, {"p1", "identityId:0x...b"}]
+	*/
+	TASK_POWER_POLICY_DATANODE_PROVIDE_POWER = 2
 
-	// dataFlow policy
+	// ==================================================================== dataFlow policy option ====================================================================
 
 )
 
 // ==================================================================== metadata policy option ====================================================================
 
 /**
-TASK_METADATA_POLICY_ROW_COLUMN
+enum:  TASK_METADATA_POLICY_ROW_COLUMN
+value: 1
+example:
+
+	{
+        "partyId": "p0",
+        "metadataId": "metadata:0xf7396b9a6be9ca6cd2099f0d5e67a748643898f2735573c0da44fc54880cf52d",
+		"metadataName" "xxx",
+        "keyColumn": 1,
+        "selectedColumns": [1, 2, 3]
+    }
+
+
 */
 type TaskMetadataPolicyRowAndColumn struct {
 	PartyId         string
@@ -64,6 +94,7 @@ type AssignmentProvidePower struct {
 	IdentityId string
 	PartyId    string
 }
+
 func (p *AssignmentProvidePower) GetIdentityId() string {
 	return p.IdentityId
 }
