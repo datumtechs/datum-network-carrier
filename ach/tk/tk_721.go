75c07a1a697b7961b7ae0fad0d0880079ed2db9f
bglmmz <lvxiaoyi@juzix.net>
2022-08-11 18:29:41 +0800

    update arg format in debug message

--- ach/tk/tk_721.go
+++ ach/tk/tk_721.go
@@ -23,23 +23,23 @@ var (
 func (m *PayAgent) inspectTk721ExtInfo(taskSponsorAddress common.Address, tk *carrierapipb.TkItem) error {
 	extInfo, err := m.getTk721ExtInfo(tk)
 	if err != nil {
-		log.WithError(err).Errorf("cannot fetch erc721 token ext info: tk:%s, id: %d", tk.TkAddress, tk.Id)
+		log.WithError(err).Errorf("cannot fetch erc721 token ext info: tk:%s, id: %s", tk.TkAddress, tk.Id)
 		return SysError
 	}
 
 	if extInfo.Owner != taskSponsorAddress {
-		log.WithError(err).Errorf("task sponsor is not the tk owner, tk: %s, id: %d", tk.TkAddress, tk.Id)
+		log.WithError(err).Errorf("task sponsor is not the tk owner, tk: %s, id: %s", tk.TkAddress, tk.Id)
 		return NotOwner
 	}
 
 	utcMilli, err := strconv.ParseInt(extInfo.Term, 10, 64)
 	if err != nil {
-		log.WithError(err).Errorf("the tk term is error, tk: %s, id: %d", tk.TkAddress, tk.Id)
+		log.WithError(err).Errorf("the tk term is error, tk: %s, id: %s", tk.TkAddress, tk.Id)
 		return TermError
 	}
 
 	if utcMilli < time.Now().UTC().UnixMilli() {
-		log.WithError(err).Errorf("the tk is Expired, tk: %s, id: %d", tk.TkAddress, tk.Id)
+		log.WithError(err).Errorf("the tk is Expired, tk: %s, id: %s", tk.TkAddress, tk.Id)
 		return TermExpired
 	}
 	/*currentBlockNo, err := m.ethContext.BlockNumber(context.Background())
