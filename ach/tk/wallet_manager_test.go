4c8ab06d5240230dff8fd1b58fc962f72895e083
bglmmz <lvxiaoyi@juzix.net>
2022-08-11 13:13:47 +0800

    add unit test

--- ach/tk/wallet_manager_test.go
+++ ach/tk/wallet_manager_test.go
@@ -1,6 +1,10 @@
 package tk
 
 import (
+	"github.com/bglmmz/chainclient"
+	"github.com/datumtechs/datum-network-carrier/carrierdb"
+	"github.com/datumtechs/datum-network-carrier/core"
+	"github.com/datumtechs/datum-network-carrier/db"
 	"github.com/ethereum/go-ethereum/common"
 	"testing"
 )
@@ -12,9 +16,30 @@ func Test_Address(t *testing.T) {
 	} else {
 		t.Log("not zero length")
 	}
-
 }
 
 func getAddress() common.Address {
 	return common.Address{}
+}
+
+func newMemoryCarrierDB() carrierdb.CarrierDB {
+	db := db.NewMemoryDatabase()
+	return core.NewDataCenter(nil, db)
+}
+func Test_InitWalletManager(t *testing.T) {
+
+	InitWalletManager(newMemoryCarrierDB(), nil)
+
+	addr, err := WalletManagerInstance().GenerateWallet()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("generate Addr:%s", addr.Hex())
+	WalletManagerInstance().loadPrivateKey()
+
+	t.Logf("load addr:%s", addr.Hex())
+	var ethContext *chainclient.EthContext
+	ethContext = chainclient.NewEthClientContext("http://8.219.126.197:6789", "lat", WalletManagerInstance())
+
+	t.Logf("ethContext.GetAddress():%s", ethContext.GetAddress().Hex())
 }
