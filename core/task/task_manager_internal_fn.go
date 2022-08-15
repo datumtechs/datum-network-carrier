878ab2e052e293a7f013538fb4a9565f463e98d7
liyuanchaoCh <a2716133731@gmail.com>
2022-08-15 17:35:18 +0800

    modify log

--- core/task/task_manager_internal_fn.go
+++ core/task/task_manager_internal_fn.go
@@ -201,7 +201,7 @@ func (m *Manager) fetchConsumeOption(localTask *types.Task) (map[uint8][]*taskCo
 				return nil, err
 			}
 			if localTask.GetTaskData().GetUser() == metadataDetail.GetData().GetUser() {
-				log.Warnf("Data owners do not need to consume credentials to perform tasks")
+				log.Warnf("Data owners do not need to consume credentials to perform tasks,metadataId is %s,partyId %s", metadataId, consumePolicy.GetPartyId())
 				continue
 			}
 			dataConsumeOptionCache, err := fetchTaskMetadataConsumeOptionsFn(consumePolicy.GetPartyId(), metadataId, consumePolicy.GetConsumeTypes(), consumePolicy.GetConsumeOptions())
